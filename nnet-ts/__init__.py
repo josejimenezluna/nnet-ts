76f72e016910e1026ce107593b6fd98330a0fe9a
hawk31 <hawk31@gmail.com>
2015-09-01 18:34:44 +0200

    deleted:    AirPassengers.csv 	deleted:    TimeSeriesNnet.py 	deleted:    nnet-ts/AirPassengers.csv 	deleted:    nnet-ts/TimeSeriesNnet.py 	deleted:    nnet-ts/TimeSeriesNnet.pyc 	deleted:    nnet-ts/__init__.py

--- nnet-ts/__init__.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import numpy as np
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Dropout
-from sklearn.preprocessing import StandardScaler
-from TimeSeriesNnet import TimeSeriesNnet
